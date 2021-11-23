33626c2228bc4c56fa5a2e3a92dbe83d899a8b77
devanshdixit01 <devanshdixit01@gmail.com>
2021-11-23 19:54:38 +0530

    changed logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ function Header(props) {
         <div className="header">
             <div className="nav-bar cols bounds">
                 <div className="title col">
-                    MafiaLords
+                    <img src="/images/logo.png" width="260" height="120" alt="" />
                 </div>
                 <div className="nav-links col">
                     <a href="#about">About MafiaLords</a>
